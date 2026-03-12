0dd8efa009c6867f31bd575a93bacf2e788b441e
S00250496 <barryryan12345@gmail.com>
2026-03-12 21:13:48 +0000

    god help me

---
+++ Assets/Scripts/Player/BasicInventory.cs
@@ -0,0 +1,21 @@
+//using NUnit.Framework;
+//using UnityEngine;
+
+//public class BasicInventory : MonoBehaviour
+//{
+//    [SerializeField] private List<string> items = new();
+
+//    public void AddItem(string item)
+//    {
+//        if (!items.Contains(item))
+//        {
+//            items.Add(item);
+//            Debug.Log($"Added {item} to inventory.");
+//        }
+//    }
+
+//    public bool HasItem(string item)
+//    {
+//        return items.Contains(item);
+//    }
+//}
