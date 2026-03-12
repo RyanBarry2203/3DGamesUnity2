887394f69bff8c1087593b950f4be3520bb15739
S00250496 <barryryan12345@gmail.com>
2026-03-12 21:30:25 +0000

    Update LightSwitch.cs

--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -16,6 +16,7 @@ public class LightSwitch : MonoBehaviour, IInteractable
     public void Interact(GameObject interactor)
     {
         IsOn = !IsOn;
+        UpdateConnectedLights();
     }
     private void UpdateConnectedLights()
     {
