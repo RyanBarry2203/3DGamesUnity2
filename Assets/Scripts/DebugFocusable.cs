56a11c0e64bf237a5007692369bfcda59b601043
S00250496 <barryryan12345@gmail.com>
2026-03-13 00:46:40 +0000

    idk anymore

--- Assets/Scripts/DebugFocusable.cs
+++ Assets/Scripts/DebugFocusable.cs
@@ -1,38 +1,73 @@
+//using UnityEngine;
+
+//public class DebugFocusable : MonoBehaviour, IFocusable
+//{
+//    [ColorUsage(true, true)] public Color highlightColor = Color.white;
+//    private Color origionalColor;
+//    private MeshRenderer meshRenderer;
+//    private Material material;
+
+//    private void Awake()
+//    {
+//        meshRenderer = GetComponent<MeshRenderer>();
+
+//        if (meshRenderer != null)
+//        {
+//            material = meshRenderer.material;
+//        }
+//    }
+//    public void Focus(GameObject interactor)
+//    {
+//        Debug.Log($"Focused on {gameObject.name} by {interactor.name}");
+
+//        if (material != null)
+//        {
+//            origionalColor = material.GetColor("_BaseColor");
+//            material.SetColor("_BaseColor", highlightColor);
+//            material.EnableKeyword("_EMISSION");
+//        }
+//    }
+//    public void Unfocus(GameObject interactor)
+//    {
+//        if (material != null)
+//        {
+//            material.SetColor("_BaseColor", origionalColor);
+//            material.DisableKeyword("_EMISSION");
+//        }
+//    }
+//}
+
+
 using UnityEngine;
 
 public class DebugFocusable : MonoBehaviour, IFocusable
 {
-    [ColorUsage(true, true)] public Color highlightColor = Color.white;
-    private Color origionalColor;
-    private MeshRenderer meshRenderer;
+    public Color highlightColor = Color.blue;
+    private Color originalColor;
     private Material material;
 
     private void Awake()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
-
-        if (meshRenderer != null)
+        if (TryGetComponent<Renderer>(out Renderer renderer))
         {
-            material = meshRenderer.material;
+            material = renderer.material;
+            originalColor = material.GetColor("_BaseColor");
         }
     }
+
     public void Focus(GameObject interactor)
     {
-        Debug.Log($"Focused on {gameObject.name} by {interactor.name}");
-
         if (material != null)
         {
-            origionalColor = material.GetColor("_BaseColor");
             material.SetColor("_BaseColor", highlightColor);
-            material.EnableKeyword("_EMISSION");
         }
     }
+
     public void Unfocus(GameObject interactor)
     {
         if (material != null)
         {
-            material.SetColor("EmissionColor", origionalColor);
-            material.DisableKeyword("_EMISSION");
+            material.SetColor("_BaseColor", originalColor);
         }
     }
 }
